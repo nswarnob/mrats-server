607e70245744eb053ccfb4f5a7a1e5df3f65b6bb
nswarnob <nswarnob@gmail.com>
2026-04-05 20:06:08 +0600

    Make Express app compatible with Vercel and add vercel.json

--- server.js
+++ server.js
@@ -459,4 +459,9 @@ app.patch(
 app.get("/", (req, res) => res.send("API is running..."));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+if (process.env.VERCEL) {
+  module.exports = app;
+} else {
+  app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+}
