862a803d3d96f7cc426a09289665f9377c1e7516
nswarnob <nswarnob@gmail.com>
2026-05-13 04:19:29 +0600

    updated

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
