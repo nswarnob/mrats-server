ee05ab413caa0b46ee6372daf687eaa30544bb8e
nswarnob <nswarnob@gmail.com>
2026-05-13 03:26:20 +0600

    fev icon status updated

--- api/index.js
+++ api/index.js
@@ -89,7 +89,7 @@ app.use(
 app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 
-app.get("/favicon.ico", (req, res) => res.status(404).end());
+app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 app.use(limiter);
 
