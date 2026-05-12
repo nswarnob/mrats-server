ea927d0f3cbb544ed491963d3193a47e4a31f1dd
nswarnob <nswarnob@gmail.com>
2026-05-13 03:41:33 +0600

    icon updated

--- api/index.js
+++ api/index.js
@@ -89,8 +89,6 @@ app.use(
 app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 
-app.get("/favicon.ico", (req, res) => res.sendStatus(204));
-
 app.use(limiter);
 
 // Connect payment routes
